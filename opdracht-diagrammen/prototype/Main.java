57c513f27eaf39a5d20c99488a1a18bc10e518d5
TheSharkysNL <mcyuillian@me.com>
2025-03-20 19:05:02 +0100

    prototype toegevoegd en container diagram aangepast

---
+++ opdracht-diagrammen/prototype/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // booking.com api call prototype
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://booking-com15.p.rapidapi.com/api/v1/attraction/getAttractionReviews?id=PR6K7ZswbGBs&page=1"))
+                .header("x-rapidapi-key", "52cd520e8cmsh67cc52752269552p165af7jsn20cf7288032d")
+                .header("x-rapidapi-host", "booking-com15.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
